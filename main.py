1b040b0d268159c9b74a2da9b09abafcb2b34c05
Teodor Chirileanu <teodorchirileanu@gmail.com>
2020-11-04 20:53:51 +0100

    fix regex to be more inclusive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-import re
+from re import findall
+from pprint import pprint
 from typing import List, Dict
 
 if __name__ == '__main__':
@@ -7,16 +7,18 @@ if __name__ == '__main__':
         data: str = file.read()
 
     apn_regex: str = r"APN Name  : ((.*))"
-    apn_name_tuples = re.findall(apn_regex, data)
+    apn_name_tuples = findall(apn_regex, data)
     apn_names: List[str] = [apn_name_tuple[0] for apn_name_tuple in apn_name_tuples]
 
     active_bearers_regex: str = r"Default bearers active: \s+ (\d+)"
-    active_bearers_nos_as_string: List[str] = re.findall(active_bearers_regex, data)
+    active_bearers_nos_as_string: List[str] = findall(active_bearers_regex, data)
     active_bearers_nos: List[int] = list(map(lambda x: int(x), active_bearers_nos_as_string))
 
-    apn_info: Dict[str, str] = {}
-    for apn_name, active_bearers_no in zip(apn_names, active_bearers_nos_as_string):
+    apn_info: Dict[str, int] = {}
+    for apn_name, active_bearers_no in zip(apn_names, active_bearers_nos):
         apn_info[apn_name] = active_bearers_no
 
-    json: str = json.dumps(apn_info, indent=4)
-    print(json)
+    # remove zeroes
+    # sort by key descending
+
+    pprint(apn_info)
